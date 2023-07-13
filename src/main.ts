3daa7fd3c51d99af7670e572251cd9b523a6c6b0
Mrigank Vallabh Singh <mrigankvallabh@gmail.com>
2023-07-13 20:42:18 +0800

    feat!:  Bootstrap the application using standalone APIs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AuthModule } from './app/auth/auth.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AuthInterceptorService } from './app/auth/auth-interceptor.service';
+import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AuthModule, AppRoutingModule),
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
